c40eae8382e35db1a5526512066cd597785688d7
gecko99 <marktrudeau1@gmail.com>
2025-05-31 19:55:19 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Set your GH Pages URL here e.g.:
-ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
+ghPagesURL = "https://gecko99.github.io/Yurrrr/"
 ghRawURL = "https://raw.githubusercontent.com/conoro/tiktok-rss-flat/main/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
